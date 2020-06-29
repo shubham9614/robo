2e7a27a43ba3f2503682cf9cd70b58d067ec2c05
shubham kaushal <64188392+shubham9614@users.noreply.github.com>
2020-06-29 19:15:20 +0530

    Add files via upload

---
+++ precache-manifest.f7e2d82f062ead7505c42475a2b98469.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67872f0eefc62f7c4d9cb2b0e2edfb9e",
+    "url": "/robo/index.html"
+  },
+  {
+    "revision": "dab360b9850c2a456077",
+    "url": "/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "da60bb89196c9f301a4c",
+    "url": "/robo/static/css/main.06bb03fa.chunk.css"
+  },
+  {
+    "revision": "dab360b9850c2a456077",
+    "url": "/robo/static/js/2.092d12b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo/static/js/2.092d12b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da60bb89196c9f301a4c",
+    "url": "/robo/static/js/main.afcf3b94.chunk.js"
+  },
+  {
+    "revision": "cd01cbebd96a4c029298",
+    "url": "/robo/static/js/runtime-main.a78c4623.js"
+  },
+  {
+    "revision": "3eb2be46a75f69a75ac6142a350c062c",
+    "url": "/robo/static/media/Patchy Robots.3eb2be46.woff"
+  }
+]);
